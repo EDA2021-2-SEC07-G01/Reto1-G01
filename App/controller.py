5affdc4bbde7e979f9be07d64e2f31196875f56b
naparicioc <n.aparicioc@uniandes.edu.co>
2021-09-22 23:59:53 -0500

    Entrega Final - Reto 1

--- App/controller.py
+++ App/controller.py
@@ -52,7 +52,7 @@ def loadArtists(catalog):
     """
     Carga los artistas del archivo y los agrega a la lista de artistas
     """
-    artistsfile = cf.data_dir + 'MoMA/Artists-utf8-small.csv'
+    artistsfile = cf.data_dir + 'MoMA/Artists-utf8-5pct.csv'
     input_file = csv.DictReader(open(artistsfile, encoding='utf-8'))
     for artist in input_file:
         model.addArtist(catalog, artist)
@@ -62,7 +62,7 @@ def loadArtworks(catalog):
     """
     Carga todas las obras de arte del archivo y las agrega a la lista de obras
     """
-    artworksfile = cf.data_dir + 'MoMA/Artworks-utf8-small.csv'
+    artworksfile = cf.data_dir + 'MoMA/Artworks-utf8-5pct.csv'
     input_file = csv.DictReader(open(artworksfile, encoding='utf-8'))
     for artwork in input_file:
         model.addArtwork(catalog, artwork)
